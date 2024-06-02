33fa2df7ceb3052ef24070001fd111ca34f62652
Serhii H <srg.gorbachov@gmail.com>
2024-06-02 14:34:16 +0300

    project init

---
+++ src/main/java/com/visitors/VisitorsReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.visitors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorsReactiveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitorsReactiveDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visitors/VisitorsReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visitors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorsReactiveDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
